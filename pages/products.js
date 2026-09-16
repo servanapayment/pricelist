60a5c69297748718fe691338454e8ca1ae650e48
servanapayment <servanapayment@gmail.com>
2026-09-17 04:55:50 +0700

    Update products.js

--- pages/products.js
+++ pages/products.js
@@ -465,8 +465,8 @@ export default function Products({ products, total, page, currentProvider, curre
                     <td className="product-name">{p.nama || p.nama_produk || "-"}</td>
                     <td>{p.provider || "-"}</td>
                     <td>Rp {p.harga_jual ? p.harga_jual.toLocaleString("id-ID") : 0}</td>
-                    <td className={p.status ? "status-open" : "status-closed"}>
-                    {p.status ? "Open" : "Closed"}
+                    <td className={p.is_active ? "status-open" : "status-closed"}>
+                    {p.is_active ? "Open" : "Closed"}
                     </td>
                   </tr>
                 ))}
